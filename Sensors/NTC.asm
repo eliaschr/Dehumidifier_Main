6ed8a1e7e3f60a042159836805657935b1bfd85a
Elias Chrysocheris <eliaschr@gmail.com>
2019-06-13 23:10:58 +0300

    Added ADC value to Temperature conversion using the NTC_Table

--- Sensors/NTC.asm
+++ Sensors/NTC.asm
@@ -57,6 +57,7 @@
 ;-------------------------------------------
 			.bss	NTCStatus, 2			;Flags that describe the status of the NTC system
 			.bss	NTCLastVal, 2			;Value fetched by the ADC
+			.bss	NTCLastTemp, 2			;Temperature value converted
 
 
 ;----------------------------------------
@@ -103,13 +104,55 @@ NTCPInit:
 ; REGS USED     : None
 ; REGS AFFECTED : None
 ; STACK USAGE   : 2 = 1x Call
-; VARS USED     : NTC_ACHANNEL
+; VARS USED     : NTC_ACHANNEL, NTC_ENABLE, NTCP_DOUT
 ; OTHER FUNCS   : ADCStartSingle
 NTCTrigger:
-			BIS.B	#NTC_ENABLE,&NTCPDOUT			;Enable the NTC divider
+			BIS.B	#NTC_ENABLE,&NTCP_DOUT			;Enable the NTC divider
 			MOV		#NTC_ACHANNEL,R10				;Channel to be sampled is the NTC one
 			CALL	#ADCStartSingle					;Start sampling of NTC divider voltage
 			RET
+
+
+;----------------------------------------
+; NTCReadTemp
+; Converts the ADC value to temperature according to NTC_Table
+; INPUT         : None
+; OUTPUT        : R4 contains the associated temperature
+; REGS USED     : R4, R12, R13, R14, R15
+; REGS AFFECTED : R4, R12, R13, R14, R15
+; STACK USAGE   : None
+; VARS USED     : NTC_CONVERTED, NTC_Table, NTC_Table_END, NTCLastTemp, NTCLastVal, NTCStatus
+; OTHER FUNCS   : None
+NTCReadTemp:
+			MOV		&NTCLastTemp,R4					;Get the possible converted value
+			BIT		#NTC_CONVERTED,&NTCStatus		;Is the value already converted?
+			JNZ		NTCRT_End						;Yes => Skip calculation
+			MOV		&NTCLastVal,R4					;Get the value in question
+			MOV		#(NTC_Table_END-NTC_Table),R15	;Maximum possible element (exclusive)
+			MOV		#00000h,R13						;Minimum possible element (inclusive)
+NTCRT_Loop:	MOV		R15,R14							;Get a copy of the maximum element
+			SUB		R13,R14							;Subtract the first
+			SHR		R14								;Divide by two to find the space between
+			BIC		#00003h,R14						;Align to DWord because the table is
+													; consisted of elements of two words each
+			MOV		R13,R12							;Get the lower element
+			ADD		R14,R12							;Add half of the space in DWords
+			CMP		#00000h,R14						;Is the distance 0 DWords?
+			JZ		NTCRT_Found						;Found the element
+			CMP		NTC_Table(R12),R4				;Compare the ADC value of this element to
+													; the one we search for
+			JEQ		NTCRT_Found						;Do we have a match? => Found!
+			JLO		NTCRT_SetHi						;Lower value? => Need to alter the hi val
+			MOV		R12,R13							;Set the lower limit
+			JMP		NTCRT_Loop						;Repeat the process until found
+NTCRT_SetHi:
+			MOV		R12,R15							;Set the higher limit
+			JMP		NTCRT_Loop						;Repeat the process until found
+NTCRT_Found:
+			MOV		NTC_Table+2(R12),R4				;Get the associated temperature value
+			BIS		#NTC_CONVERTED,&NTCStatus		;The last value is already converted
+			MOV		R4,&NTCLastTemp					;Store the calculated value
+NTCRT_End:	RET										;Return to caller
 
 
 ;----------------------------------------
@@ -129,8 +172,11 @@ NTCTrigger:
 ; OTHER FUNCS   : None
 NTCCallback:
 			BIC.B	#NTC_ENABLE,&NTCP_DOUT			;Disable the NTC subsystem
+			CMP		R4,&NTCLastVal					;Is the new value equal to the last one?
+			JZ		NTCCB_Skip						;Yes => No need to store it
 			MOV		R4,&NTCLastVal					;Store the new value
-			BIS		#NTC_READY,&NTCStatus			;Flag that there is a new value stored
+			BIC		#NTC_CONVERTED,&NTCStatus		;Need to convert the new value
+NTCCB_Skip:	BIS		#NTC_READY,&NTCStatus			;Flag that there is a new value stored
 			SETC									;Flag the need to wake the system up, to
 													; the calling ISR
 			RET
